f36c0a489bd7c54ef2a979ac8cafd5e7848a22ff
Steven <stevengf14@gmail.com>
2023-12-14 21:16:20 -0500

    Alternative props syntaxes

---
+++ react-essentials/starting-project/src/data.js
@@ -0,0 +1,27 @@
+import componentsImg from "./assets/components.png";
+import propsImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
+import stateImg from "./assets/state-mgmt.png";
+
+export const CORE_CONCEPTS = [
+  {
+    image: componentsImg,
+    title: "Components",
+    description: "The core UI building block - compose the user interface by",
+  },
+  {
+    image: jsxImg,
+    title: "JSX",
+    description: "Return (potentially dynamic) HTML(ish) code to define the",
+  },
+  {
+    image: propsImg,
+    title: "Props",
+    description: "The core UI building block - compose the user interface by",
+  },
+  {
+    image: stateImg,
+    title: "State",
+    description: "The core UI building block - compose the user interface by",
+  },
+];
