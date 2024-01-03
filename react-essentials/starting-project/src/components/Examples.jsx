188c28e918e815493c7d1c959b3592825f6a369e
Steven <stevengf14@gmail.com>
2024-01-03 00:24:19 -0500

    Section component

--- react-essentials/starting-project/src/components/Examples.jsx
+++ react-essentials/starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton";
+import Section from "./Section";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -23,8 +24,7 @@ export default function Examples() {
     );
   }
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
@@ -52,6 +52,6 @@ export default function Examples() {
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
