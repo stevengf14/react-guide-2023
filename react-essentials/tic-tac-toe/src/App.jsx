ec5e28f9941305958ffabee372407f65e7aa9877
Steven <stevengf14@gmail.com>
2024-01-04 22:21:13 -0500

    First step towards tic-tac-toe game

--- react-essentials/tic-tac-toe/src/App.jsx
+++ react-essentials/tic-tac-toe/src/App.jsx
@@ -1,10 +1,18 @@
-
 function App() {
-  
-
   return (
-    <p>Comming soon...</p>
-  )
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
-export default App
+export default App;
