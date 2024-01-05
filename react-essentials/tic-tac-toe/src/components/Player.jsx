2c8e330ff67886c1d090353539bb6402dae73ca8
Steven <stevengf14@gmail.com>
2024-01-04 22:35:44 -0500

    working with state

--- react-essentials/tic-tac-toe/src/components/Player.jsx
+++ react-essentials/tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleClick = () => {
+    setIsEditing(true);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>Edit</button>
     </li>
   );
 }
