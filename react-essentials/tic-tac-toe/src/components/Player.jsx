fc34a130b2de50e84b47f2ebb9449ae22264add2
Steven <stevengf14@gmail.com>
2024-01-05 21:02:45 -0500

    best practice

--- react-essentials/tic-tac-toe/src/components/Player.jsx
+++ react-essentials/tic-tac-toe/src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   };
 
   return (
