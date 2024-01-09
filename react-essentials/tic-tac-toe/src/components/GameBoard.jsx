3471eb29464a3d3167dd9f05dff7b0355ea224e4
Steven <stevengf14@gmail.com>
2024-01-08 22:06:32 -0500

    Lifting state up

--- react-essentials/tic-tac-toe/src/components/GameBoard.jsx
+++ react-essentials/tic-tac-toe/src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initalGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initalGameBoard);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
@@ -14,9 +14,11 @@ export default function GameBoard() {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare();
   };
 
   return (
