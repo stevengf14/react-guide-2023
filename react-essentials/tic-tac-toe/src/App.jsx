d2b2e6987501854587e7ab534928c7fdca0836f4
Steven <stevengf14@gmail.com>
2024-01-10 22:01:14 -0500

    Game over screen & checking for a draw!

--- react-essentials/tic-tac-toe/src/App.jsx
+++ react-essentials/tic-tac-toe/src/App.jsx
@@ -4,6 +4,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initalGameBoard = [
   [null, null, null],
@@ -52,6 +53,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = derivaActivePlayer(prevTurns);
@@ -78,7 +81,7 @@ function App() {
             isActive={activePlayer === "O"}
           ></Player>
         </ol>
-        {winner && <p>YOU WON, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
