58a5ff6582e128d26a516b73e8882eceee0e7bb2
Steven <stevengf14@gmail.com>
2024-01-17 23:14:16 -0500

    Dynamic & conditional styling with tailwind

--- react-essentials/styling-start/src/components/Input.jsx
+++ react-essentials/styling-start/src/components/Input.jsx
@@ -1,37 +1,18 @@
-import styled from "styled-components";
+export default function Input({ label, invalid, ...props }) {
+  const labelClasses = `block mb-2 text-xs font-bold tracking-wide uppercase ${
+    invalid ? "text-red-400" : "text-stone-300"
+  }`;
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
+  const inputClasses = `w-full px-3 py-2 leading-tight  border rounded shadow ${
+    invalid
+      ? "text-red-500 bg-red-100 border-red-300"
+      : "text-gray-700 bg-stone-300"
+  }`;
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  ${({ $invalid }) =>
-    $invalid
-      ? `color: #ef4444;
-      border-color: #f73f3f;
-      background-color: #fed2d2;`
-      : ""}}
-`;
-
-export default function CustomInput({ label, invalid, ...props }) {
   return (
     <p>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...props} />
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
     </p>
   );
 }
