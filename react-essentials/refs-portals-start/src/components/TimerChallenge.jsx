730e51ede15bb9891e455bbc09831ce8a7bb713c
Steven <stevengf14@gmail.com>
2024-01-24 22:11:17 -0500

    using refs for more than "DOM element connections"

--- react-essentials/refs-portals-start/src/components/TimerChallenge.jsx
+++ react-essentials/refs-portals-start/src/components/TimerChallenge.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimerExpired(true);
+    setTimeout(() => {}, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop(){
+    
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running.../Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 }
