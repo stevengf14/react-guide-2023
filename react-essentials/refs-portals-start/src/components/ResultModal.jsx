fea50b5729dda01df3326e66b02f50fef86acdb8
Steven <stevengf14@gmail.com>
2024-01-25 21:34:30 -0500

    Sharing state across components

--- react-essentials/refs-portals-start/src/components/ResultModal.jsx
+++ react-essentials/refs-portals-start/src/components/ResultModal.jsx
@@ -1,17 +1,21 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { result, targetTime, remainingTime, onRest },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   useImperativeHandle(ref, () => {
     return {
       open: () => {
         try {
           dialog.current.showModal();
         } catch (error) {
-          console.log("Couldn't open modal or modal is already open.");
+          console.log("Couldn't open modal or modal is already open.", error);
         }
       },
     };
@@ -19,14 +23,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={ref} className="result-modal">
-      <h2>Your {result}</h2>
+      {userLost && <h2>Your {result}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onRest}>
         <button>Close</button>
       </form>
     </dialog>
