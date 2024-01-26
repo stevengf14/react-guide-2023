8defbaf44f4b89425de55887a774624831414fc9
Steven <stevengf14@gmail.com>
2024-01-25 21:37:18 -0500

    Enhacing the Demo App "Result Modal"

--- react-essentials/refs-portals-start/src/components/ResultModal.jsx
+++ react-essentials/refs-portals-start/src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime*1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -23,7 +24,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={ref} className="result-modal">
-      {userLost && <h2>Your {result}</h2>}
+      {userLost ? <h2>Your {result}</h2> : <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
