720d348cabb266de4093ddbedaa68c08f7955ce9
Steven <stevengf14@gmail.com>
2024-01-29 22:10:57 -0500

    Handling project creation & updating the UI

--- react-essentials/refs-demo-project/src/App.jsx
+++ react-essentials/refs-demo-project/src/App.jsx
@@ -19,33 +19,35 @@ function App() {
   }
 
   function handleAddProject(projectData) {
+    console.log(projectData);
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
-    content = (
-      <NoProjectSelected onStartAddProject={{ handleStartAddProject }} />
-    );
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
