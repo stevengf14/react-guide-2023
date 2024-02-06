f2347cf56af9f7800b4d8bed5860b62c658673db
Steven <stevengf14@gmail.com>
2024-02-05 21:18:33 -0500

    Not all side effects need useEffect

--- react-essentials/effects-start/src/App.jsx
+++ react-essentials/effects-start/src/App.jsx
@@ -41,6 +41,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = localStorage.getItem("selectedPlaces") || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
