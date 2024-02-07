9bd3dd7b15ac68602eb473490651681336ca9920
Steven <stevengf14@gmail.com>
2024-02-06 21:52:02 -0500

    Preparing another problem that can be fixed with useEffect

--- react-essentials/effects-start/src/components/DeleteConfirmation.jsx
+++ react-essentials/effects-start/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,9 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  console.log("TIMER SET");
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
