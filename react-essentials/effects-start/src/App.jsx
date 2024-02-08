b0a466d14f0e3cdd13510eda269c73088b17df49
Steven <stevengf14@gmail.com>
2024-02-07 22:05:51 -0500

    The useCallback Hook

--- react-essentials/effects-start/src/App.jsx
+++ react-essentials/effects-start/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -61,7 +61,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -73,7 +73,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(parsedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
