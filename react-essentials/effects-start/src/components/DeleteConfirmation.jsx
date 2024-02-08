6bf308009b379bc854b2f2fd2801308f2cd87b0a
Steven <stevengf14@gmail.com>
2024-02-07 21:47:31 -0500

    Introducing useEffect's cleanup function

--- react-essentials/effects-start/src/components/DeleteConfirmation.jsx
+++ react-essentials/effects-start/src/components/DeleteConfirmation.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  console.log("TIMER SET");
-  setTimeout(() => {
-    onConfirm();
-  }, 3000);
+  useEffect(() => {
+    console.log("TIMER SET");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      console.log("Cleaing up timer")
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div id="delete-confirmation">
