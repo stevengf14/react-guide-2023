8061b116c89582ad3ebb1230926875218c8e6118
Steven <stevengf14@gmail.com>
2024-02-07 22:01:40 -0500

    The problem with object & function dependencies

--- react-essentials/effects-start/src/components/DeleteConfirmation.jsx
+++ react-essentials/effects-start/src/components/DeleteConfirmation.jsx
@@ -11,7 +11,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
       console.log("Cleaing up timer")
       clearTimeout(timer);
     };
-  }, []);
+  }, [onConfirm]);
 
   return (
     <div id="delete-confirmation">
