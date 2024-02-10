e00e9bd9ab9f798e87b3345500e89647e1651be9
Steven <stevengf14@gmail.com>
2024-02-09 21:44:01 -0500

    Moving logic to components that actually need it ("moving state down")

--- react-essentials/quiz-project/src/components/Answers.jsx
+++ react-essentials/quiz-project/src/components/Answers.jsx
@@ -29,7 +29,11 @@ export default function Answers({
         }
         return (
           <li key={answer} className="answer">
-            <button onClick={() => onSelect(answer)} className={cssClasses}>
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClasses}
+              disabled={answerState !== ""}
+            >
               {answer}
             </button>
           </li>
--- react-essentials/quiz-project/src/components/Quiz.jsx
+++ react-essentials/quiz-project/src/components/Quiz.jsx
@@ -1,39 +1,23 @@
 import { useState, useCallback } from "react";
+
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answers from "./Answers.jsx";
-import Question from "./Questions.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUsersAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUsersAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
-
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUsersAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -54,10 +38,7 @@ export default function Quiz() {
       <div id="question">
         <Question
           key={activeQuestionIndex}
-          questionText={QUESTIONS[activeQuestionIndex].text}
-          answers={QUESTIONS[activeQuestionIndex].answers}
-          answerState={answerState}
-          selectedAnswer={userAnswers[userAnswers.length - 1]}
+          index={activeQuestionIndex}
           onSelectAnswer={handleSelectAnswer}
           onSkipAnswer={handleSkipAnswer}
         />
