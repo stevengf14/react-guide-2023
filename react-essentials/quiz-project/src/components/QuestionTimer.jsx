b9c7263fc3ed99f67268274f634b4c6682bb78b6
Steven <stevengf14@gmail.com>
2024-02-09 20:37:54 -0500

    Working with effect dependencies & useCallback

--- react-essentials/quiz-project/src/components/QuestionTimer.jsx
+++ react-essentials/quiz-project/src/components/QuestionTimer.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
-  setTimeout(onTimeout, timeout);
-
   useEffect(() => {
+    console.log("SETTING TIMEOUT");
     setTimeout(onTimeout, timeout);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
+    console.log("SETTING INTERVAL");
     setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
