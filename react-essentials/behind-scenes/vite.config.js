a915508d1b6dfca291f6970e3456380412a68680
Steven <stevengf14@gmail.com>
2024-02-15 22:20:50 -0500

    Optimizing react with MillionJS

--- react-essentials/behind-scenes/vite.config.js
+++ react-essentials/behind-scenes/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import million from "million/compiler";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [million.vite({ auto: true }), react()],
 });
