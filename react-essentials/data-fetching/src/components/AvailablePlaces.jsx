9f2a6348f6e3372c6dcf39032a34439d12b25bba
Steven <stevengf14@gmail.com>
2024-02-20 21:57:09 -0500

    How not to send http request (and why it's wrong)

--- react-essentials/data-fetching/src/components/AvailablePlaces.jsx
+++ react-essentials/data-fetching/src/components/AvailablePlaces.jsx
@@ -5,6 +5,14 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
   return (
     <Places
       title="Available Places"
