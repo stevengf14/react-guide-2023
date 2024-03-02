3525688b6aafa7795bdd8a624e9bb8f6b689dc04
Steven <stevengf14@gmail.com>
2024-03-01 21:24:47 -0500

    Adding a "MealItem" component

--- react-essentials/food-order/backend/app.js
+++ react-essentials/food-order/backend/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
