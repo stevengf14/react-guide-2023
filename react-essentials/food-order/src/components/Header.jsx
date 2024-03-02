524d500fe631065f7e4628a5183fa5f42839054f
Steven <stevengf14@gmail.com>
2024-03-01 19:39:38 -0500

    Planning the app & adding a first component

---
+++ react-essentials/food-order/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoImage from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImage} alt="A restaurant" />
+        <h1>React Food</h1>
+      </div>
+      <nav>
+        <button>Cart(0)</button>
+      </nav>
+    </header>
+  );
+}
