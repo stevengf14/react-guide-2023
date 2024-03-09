82bd956fd687096027c02ada8c6ed2b68d358c71
Steven <stevengf14@gmail.com>
2024-03-08 22:53:46 -0500

    More redux basics concepts

--- react-essentials/redux-demo/redux-demo.js
+++ react-essentials/redux-demo/redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -16,3 +24,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
