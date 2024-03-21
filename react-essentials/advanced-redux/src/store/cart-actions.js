73328b12f20fe64a038c7263a8047dffcf298ba9
Steven <stevengf14@gmail.com>
2024-03-20 22:21:59 -0500

    Finalizing the Fetching Logic

--- react-essentials/advanced-redux/src/store/cart-actions.js
+++ react-essentials/advanced-redux/src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://advanced-redux-66c12-default-rtdb.firebaseio.com/cart.json"
+        ""
       );
 
       if (!response.ok) {
@@ -18,7 +18,12 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -42,8 +47,14 @@ export const sendCartData = (cart) => {
     );
     const sendRequest = async () => {
       const response = await fetch(
-        "https://advanced-redux-66c12-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        "",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
