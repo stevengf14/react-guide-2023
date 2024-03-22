7e6d10fffc868e40a621b91e76036fc386cd7ac9
Steven <stevengf14@gmail.com>
2024-03-21 23:12:09 -0500

    fixing version

--- react-essentials/spa/src/App.js
+++ react-essentials/spa/src/App.js
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 
 const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
