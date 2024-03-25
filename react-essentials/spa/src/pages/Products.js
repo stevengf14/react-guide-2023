beefc4c898f647f366bdd2809828e11ce14617ae
Steven <stevengf14@gmail.com>
2024-03-25 15:52:53 -0500

    Understanding relative & absolute paths

--- react-essentials/spa/src/pages/Products.js
+++ react-essentials/spa/src/pages/Products.js
@@ -22,7 +22,9 @@ function ProductsPage() {
       <ul>
         {PRODUCTS.map((prod) => (
           <li key={prod.id}>
-            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+            <Link to={prod.id} relative="path">
+              {prod.title}
+            </Link>
           </li>
         ))}
       </ul>
