8c7352fd651eabb705c179413caa628b5b362589
Steven <stevengf14@gmail.com>
2024-03-26 23:39:12 -0500

    Data Fetching with a loader()

--- react-essentials/advanced-spa/frontend/src/pages/Events.js
+++ react-essentials/advanced-spa/frontend/src/pages/Events.js
@@ -1,45 +1,28 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const EVENTS = [
-  {
-    id: "e1",
-    title: "First Dummy Event!",
-    description: "This is the first dummy event! Enjoy it",
-  },
-  {
-    id: "e2",
-    title: "Second Dummy Event!",
-    description: "This is the second dummy event! Enjoy it",
-  },
-  {
-    id: "e3",
-    title: "Third Dummy Event!",
-    description: "This is the Thrid dummy event! Enjoy it",
-  },
-  {
-    id: "e4",
-    title: "Fourth Dummy Event!",
-    description: "This is the first dummy event! Enjoy it",
-  },
-  {
-    id: "e5",
-    title: "Fifth Dummy Event!",
-    description: "This is the fifth dummy event! Enjoy it",
-  },
-];
+import EventsList from "../components/EventsList";
+
 function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>Events</h1>
-      <ul>
-        {EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id} relative="path">
-              {event.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 }
