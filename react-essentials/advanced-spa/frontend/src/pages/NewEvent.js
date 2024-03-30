e476c1915e33e946ba1198740114ec3b987e2b6a
Steven <stevengf14@gmail.com>
2024-03-29 23:23:55 -0500

    Working with action() functions

--- react-essentials/advanced-spa/frontend/src/pages/NewEvent.js
+++ react-essentials/advanced-spa/frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
@@ -5,3 +6,28 @@ function NewEventPage() {
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+  console.log(response.ok);
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+  return redirect("/events");
+}
