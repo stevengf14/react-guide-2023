0ae26d976f01d4499ec834c44d7a1bc4920fb7b5
Steven <stevengf14@gmail.com>
2024-11-28 22:55:24 -0500

    Submitting Data Programmatically

--- react-essentials/advanced-spa/frontend/src/pages/EventDetail.js
+++ react-essentials/advanced-spa/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, useRouteLoaderData, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
@@ -25,4 +25,16 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete event." }, { status: 500 });
+  }
+  return redirect("/events");
 }
