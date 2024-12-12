2e7e8817e0b093b0f87e6a54e7a2d6728d2d44b3
Steven <stevengf14@gmail.com>
2024-12-11 22:35:24 -0500

    Reusing actions via request methods

--- react-essentials/advanced-spa/frontend/src/App.js
+++ react-essentials/advanced-spa/frontend/src/App.js
@@ -10,6 +10,7 @@ import EventDetailPage, {
 import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 const router = createBrowserRouter([
   {
@@ -37,10 +38,18 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deleteEventAction,
               },
-              { path: "edit", element: <EditEventPage /> },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
       },
